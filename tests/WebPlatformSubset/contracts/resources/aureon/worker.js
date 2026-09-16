02d3ddc72e28f93909d248370d2db9725f1849de
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-09-16 02:56:24 +0200

    Expose dedicated worker global scope identity

--- tests/WebPlatformSubset/contracts/resources/aureon/worker.js
+++ tests/WebPlatformSubset/contracts/resources/aureon/worker.js
@@ -1,6 +1,21 @@
-import { count } from './module.js';
+const isWebWorker = typeof self === 'object' && self.constructor
+    && self.constructor.name === 'DedicatedWorkerGlobalScope';
+const { count } = await import(isWebWorker ? './module.js' : 'module');
 self.onmessage = e => {
     e.data[0] += count;
     postMessage(e.data, [e.data.buffer]);
-    postMessage({ detached: e.data.byteLength === 0, documentType: typeof document });
+    let workerScopeConstructorsReject = false;
+    try { new DedicatedWorkerGlobalScope(); } catch (error) {
+        workerScopeConstructorsReject = error instanceof TypeError;
+    }
+    postMessage({
+        detached: e.data.byteLength === 0,
+        documentType: typeof document,
+        isWebWorker,
+        dedicatedIdentity: self instanceof DedicatedWorkerGlobalScope,
+        workerIdentity: self instanceof WorkerGlobalScope,
+        eventTargetIdentity: self instanceof EventTarget,
+        stringTag: Object.prototype.toString.call(self),
+        workerScopeConstructorsReject
+    });
 };
